b3e8675f342a6f38c0b4a0396daf4bdd17c72730
Paul Trotter <paul.trotter9@gmail.com>
2023-05-14 13:30:55 +0000

    deleted disable static env variable

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -179,7 +179,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
-    # Bucket Config
+    # Bucket Configuration
     AWS_STORAGE_BUCKET_NAME = 'boutique-ado-v1-patiat'
     AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
